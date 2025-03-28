dd77dbece7a93e596c120bec7afc14d9866a67ab
Satheesh Revoori <104457018+satheeshrevoori@users.noreply.github.com>
2025-03-28 18:28:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function highlight() {
     //Write your code here
+	 var paraEle = document.getElementById("para");
+            var strongEle = document.querySelectorAll('strong');
+            strongEle.forEach((el) => { el.style.color = "rgb(0, 128, 0)"; })
 
 
 }
@@ -7,6 +10,7 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
-
+var strongEle = document.querySelectorAll('strong');
+            strongEle.forEach((el) => { el.style.color = "rgb(0, 0, 0)"; })
     
 }
